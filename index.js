53a4a6fba52ba1c0f7697d464bcf73417b586ee7
Duy Nguyen <nguyenduy010798@gmail.com>
2020-07-24 14:01:31 +0300

    asad

--- index.js
+++ index.js
@@ -8,21 +8,20 @@ const Profile = require("./ProfileSchema");
 app.use(bodyParser.json());
 
 app.get("/", async (req, res) => {
-  const profile = await Profile.find();
-  res.json(profile);
-  res.end();
+  res.send(process.env.MONDGODB_URI);
 });
 
 mongoose.connect(
   process.env.MONDGODB_URI ||
     "mongodb+srv://wyfy:Wyfy010798@cluster0.juxbs.mongodb.net/Cluster0?retryWrites=true&w=majority",
   { useNewUrlParser: true, useUnifiedTopology: true },
-  async (err, res) => {
-    try {
-      console.log('success')
-    } catch (error) {
-      res.json({message: error})
+  (err, res) => {
+    if (err) {
+      console.log("failed to" + error);
+    } else {
+      console.log("success");
     }
+  }
 );
 
 app.listen(PORT);
