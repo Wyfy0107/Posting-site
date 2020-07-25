ae8c396784ef5d1cfa1e20acb1b58bdf67198349
Duy Nguyen <nguyenduy010798@gmail.com>
2020-07-25 23:37:42 +0300

    added cors

--- index.js
+++ index.js
@@ -4,6 +4,9 @@ const bodyParser = require("body-parser");
 const app = express();
 const PORT = process.env.PORT || 5000;
 const Post = require("./PostSchema");
+const cors = require("cors");
+
+app.use(cors());
 
 app.use(bodyParser.json());
 
